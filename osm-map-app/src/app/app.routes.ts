aa41746d1541ebd718a229af1a3fe51cd2548597
AXD <axd@maaster.bg>
2025-02-13 16:41:40 +0300

    прикручена карта

--- osm-map-app/src/app/app.routes.ts
+++ osm-map-app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
+import {MapComponent} from './map/map.component';
 
-export const routes: Routes = [];
+
+export const routes: Route[] = [
+  { path: 'map', component: MapComponent },
+  { path: '', redirectTo: '/map', pathMatch: 'full' }
+];
--- osm-map-app/src/app/map/map.component.ts
+++ osm-map-app/src/app/map/map.component.ts
@@ -1,43 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import * as L from 'leaflet';
+import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
-  private map!: L.Map; // Используем non-null assertion
-  private markers: L.LatLng[] = []; // Массив для хранения координат маркеров
+export class MapComponent implements AfterViewInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
 
-  ngOnInit(): void {
-    this.initMap();
-  }
+  async ngAfterViewInit(): Promise<void> {
+    if (isPlatformBrowser(this.platformId)) {
+      const L = await import('leaflet');
 
-  private initMap(): void {
-    this.map = L.map('map').setView([55.751244, 37.618423], 13); // Москва
+      const map = L.map('map').setView([51.505, -0.09], 13);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '© OpenStreetMap contributors'
-    }).addTo(this.map);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '© OpenStreetMap contributors'
+      }).addTo(map);
 
-    // Добавляем обработчик кликов для добавления маркеров и линий
-    this.map.on('click', (e) => this.addMarkerAndLine(e));
-  }
-
-  private addMarkerAndLine(e: L.LeafletMouseEvent): void {
-    const latlng = e.latlng;
-
-    // Создаём новый маркер
-    const marker = L.marker(latlng).addTo(this.map);
-
-    // Добавляем координаты в массив
-    this.markers.push(latlng);
-
-    // Если маркеров больше одного, рисуем линию
-    if (this.markers.length > 1) {
-      L.polyline(this.markers, { color: 'red' }).addTo(this.map);
+      L.marker([51.5, -0.09]).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     }
   }
 }
